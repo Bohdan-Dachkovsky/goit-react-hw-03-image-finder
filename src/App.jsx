4b02660d8988d6403ea01a93b82ae38d9e7c923f
Bohdan-Dachkovsky <90333649+Bohdan-Dachkovsky@users.noreply.github.com>
2022-05-29 18:42:21 +0300

    Eror in name component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 // import { v4 as uuidv4 } from 'uuid';
 // import  from './ContactList/ContactList';
-import { Button } from 'components/Button/Button';
-import { ImagineGallery } from 'components/ImagineGallary/ImagineGallary';
-import { ImagineGalleryItem } from 'components/ImagineGallaryItem/ImagineGallaryItem';
+import Button from 'components/Button/Button';
+import ImagineGallery from 'components/ImagineGallery/ImagineGallery';
+import ImagineGalleryItem from 'components/ImagineGalleryItem/ImagineGalleryItem';
 // import Loader from 'components/Loader/Loader';
-import { Modal } from 'components/Modal/Modal';
-import { Searchbar } from 'components/Searchbar/Searchbar';
+import Modal from 'components/Modal/Modal';
+import Searchbar from 'components/Searchbar/Searchbar';
 import 'index.css';
 class App extends Component {
   render() {
