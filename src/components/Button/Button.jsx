b810c2c5c9574a4625aa91b02c81a6bec250985d
Bohdan-Dachkovsky <90333649+Bohdan-Dachkovsky@users.noreply.github.com>
2022-05-29 19:44:25 +0300

    Commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 // import styles from './ContactForm.module.css';
-
+import bcss from './button.module.css';
 export default class Button extends Component {
   render() {
-    return <button type="submit">Add contact</button>;
+    return (
+      <button className={bcss.utton} type="submit">
+        Add contact
+      </button>
+    );
   }
 }
