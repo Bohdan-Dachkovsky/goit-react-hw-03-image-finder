7c128d354d072587e2df3b4430c0ec0bd932df4d
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2022-11-25 18:41:46 +0200

    Update ImagineGallery.jsx

--- src/components/ImagineGallery/ImagineGallery.jsx
+++ src/components/ImagineGallery/ImagineGallery.jsx
@@ -2,19 +2,21 @@ import css from './imageGallery.module.css'
 import PropTypes from 'prop-types'
 
 const ImagineGallery = ({ onShow, items, error, basement, children }) => {
-  const photos = items.map(({ id, largeImageURL, webformatURL, tags }) => (
-    <div
-      key={id}
-      onClick={() => onShow({ largeImageURL, tags })}
-      className={css.box}
-    >
-      <ul className={css.list}>
-        <li className={css.el}>
-          <img src={webformatURL} alt={tags} />
-        </li>
-      </ul>
-    </div>
-  ))
+  const photos = (items || []).map(
+    ({ id, largeImageURL, webformatURL, tags }) => (
+      <div
+        key={id}
+        onClick={() => onShow({ largeImageURL, tags })}
+        className={css.box}
+      >
+        <ul className={css.list}>
+          <li key={tags} className={css.el}>
+            <img src={webformatURL} alt={tags} />
+          </li>
+        </ul>
+      </div>
+    ),
+  )
   return (
     <div className={css.distance}>
       {error && console.log('Виникла помилка, cпробуйте будь ласка пізніше')}
@@ -27,19 +29,11 @@ const ImagineGallery = ({ onShow, items, error, basement, children }) => {
 }
 export default ImagineGallery
 ImagineGallery.defaultProps = {
-  items: [],
   searchName: '',
 }
 
 ImagineGallery.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-    }),
-  ),
+  items: PropTypes.array.isRequired,
   error: PropTypes.string,
   onLoader: PropTypes.func.isRequired,
   onBox: PropTypes.func.isRequired,
